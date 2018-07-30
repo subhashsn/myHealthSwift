b1dc7bcd681eb7957795a21dfe302759c199c765
subhash <snsubhash78@gmail.com>
2018-07-30 12:15:01 +0530

    Initial setup

---
+++ MyHealthProj/Dashboard/Controller/dashboardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  dashboardCollectionViewCell.swift
+//  MyHealthProj
+//
+//  Created by Subhash on 13/07/18.
+//  Copyright © 2018 Subhash. All rights reserved.
+//
+
+import UIKit
+
+class dashboardCollectionViewCell: UICollectionViewCell {
+    
+}
